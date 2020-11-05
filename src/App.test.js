6271a68bec8036dc6d9639cf4eea9360f00d9f02
edensongjbs <edensongjbs@gmail.com>
2020-11-05 13:03:29 -0500

    write first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Enzyme from 'enzyme'
+import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({adapter: new EnzymeAdapter()})
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test ('renders without crashing', () => {
+  const wrapper = shallow(<App/>)
+  console.log(wrapper.debug())
+  expect(wrapper).toBeTruthy()
+})
