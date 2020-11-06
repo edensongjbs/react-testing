732bff4bc5fb18f9183f454ddd16e10f64c640b4
edensongjbs <edensongjbs@gmail.com>
2020-11-06 10:58:42 -0500

    comment out react testing lib

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
 import Enzyme, {shallow} from 'enzyme'
 import EnzymeAdapter from 'enzyme-adapter-react-16'
